023f369aa55fccaf788e7ad47da321444d3af012
nmd001 <nmd001@lyn188-06>
2017-01-30 16:49:16 -0500

    bhk

--- Assignement 2/src/Main.java
+++ Assignement 2/src/Main.java
@@ -3,25 +3,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
 
 import com.opencsv.CSVReader;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException{
+	public static void main(String[] args) throws IOException{
 		CSVReader in = new CSVReader(new FileReader(new File("Student.csv")), ',' , '"');
-		DataOutputStream out = new DataOutputStream(new FileOutputStream("out.txt", true));
+		DataOutputStream out = new DataOutputStream(new FileOutputStream("out.txt"));
+		Iterator<String[]> it = in.iterator();
 		
-		while(true){
-			try{
-				String[] arr = in.readNext();
-				for(String s: arr){
-					out.writeUTF(s);
-				}
-			} catch(Exception e){
-				e.printStackTrace(System.out);
-				break;
-			}
+		
+		while(it.hasNext()){
+			String[] arr = it.next();
+			
+			if(arr == null) break;
+			for(String s: arr){
+				out.writeChars(s);
+			}	
 		}
 	}
 	
