7e95350b8c310bfe0d91bdaaff24b64b5dd3da84
nmd001 <nmd001@lyn188-06>
2017-01-30 16:12:07 -0500

    sdfg

--- Assignement 2/src/Main.java
+++ Assignement 2/src/Main.java
@@ -1,14 +1,27 @@
-import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import 
+import java.io.FileOutputStream;
+import java.io.FileReader;
+
+import com.opencsv.CSVReader;
 
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException{
-		CSVParser csvIn = new CSVParser("Student.csv", DEFAULT)
-				
+		CSVReader in = new CSVReader(new FileReader(new File("Student.csv")), ',' , '"');
+		DataOutputStream out = new DataOutputStream(new FileOutputStream("out.txt"));
+		
+		while(true){
+			try{
+				String[] arr = in.readNext();
+				for(String s: arr){
+					out.writeUTF(s);
+				}
+			} catch(Exception e){
+				break;
+			}
+		}
 	}
 	
 }
