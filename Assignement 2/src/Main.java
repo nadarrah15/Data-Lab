295b08d5c8129ff026568f05f8618da78424f5f1
nmd001 <nmd001@lyn188-06>
2017-01-30 15:27:12 -0500

    asd

--- Assignement 2/src/Main.java
+++ Assignement 2/src/Main.java
@@ -1,4 +1,13 @@
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import 
 
 public class Main {
 
+	public static void main(String[] args) throws FileNotFoundException{
+		CSVParser csvIn = new CSVParser("Student.csv", DEFAULT)
+	}
+	
 }
