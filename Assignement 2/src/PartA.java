30e5bb96c3184e27a75e5b6194d4b2f89b0e9232
nmd001 <nmd001@lyn188-06>
2017-02-01 15:51:13 -0500

    k

--- Assignement 2/src/PartA.java
+++ Assignement 2/src/PartA.java
@@ -8,14 +8,13 @@ import java.io.IOException;
 import java.util.Iterator;
 import com.opencsv.CSVReader;
 
-public class Main {
+public class PartA {
 
 	public static void main(String[] args) throws IOException{
 		File file = new File("Student.csv");
 		CSVReader in = new CSVReader(new FileReader(file), ',' , '"');
 		DataOutputStream out = new DataOutputStream(new FileOutputStream("out.bin"));
 		Iterator<String[]> it = in.iterator();
-		
 		
 		while(it.hasNext()){
 			String[] arr = it.next();
@@ -29,7 +28,18 @@ public class Main {
 		DataInputStream data = new DataInputStream(new FileInputStream(file));
 		String s = data.readUTF();
 		
-		
+		System.out.println(s);
 	}
 	
+	static boolean isDouble(String str) {
+		
+		try {
+			Double.parseDouble(str);
+		} 
+		catch (NumberFormatException e) {
+			return false;
+		}
+
+		return true;
+	}
 }
