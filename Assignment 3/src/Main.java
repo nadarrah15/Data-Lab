b32afb41bcfcc3bf3f660013786422dd9e375659
nmd001 <nmd001@lyn188-06>
2017-02-27 16:49:20 -0500

    k

--- Assignment 3/src/Main.java
+++ Assignment 3/src/Main.java
@@ -14,29 +14,29 @@ public class Main {
 		
 		BufferedReader in = new BufferedReader(new FileReader(new File("Student.csv")));
 		
-		ArrayList<Record> map = readCsv(in);
+		ArrayList<Map<String, Object>> map = readCsv(in);
 	}
 	
-	static ArrayList<Record> readCsv(BufferedReader in) throws IOException {
+	static ArrayList<Map<String, Object>> readCsv(BufferedReader in) throws IOException {
 
 		// holds column names
 		ArrayList<String> colNames = new ArrayList<String>();
 		for(String s : in.readLine().split(","))
 			colNames.add(s);
 		
-		ArrayList<Record> dict = new ArrayList<Record>();
+		ArrayList<Map<String, Object>> dict = new ArrayList<Map<String, Object>>();
 		
 		while(in.ready()){
 			
-			String[] input = in.readLine().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+			//splits the next line by commas and keeps commas within quotes (example: "name, major, "street, city"" is split into "name", "major", "street, city")
 			
-			Address address = new Address(input[colNames.indexOf("Address.Street")], input[colNames.indexOf("Address.City")], 
+			String[] input = in.readLine().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+				
+			
+			Address address = new Address(input[colNames.indexOf("Address.Street")], input[colNames.indexOf("Address.city")], 
 					input[colNames.indexOf("Address.State")], Integer.parseInt(input[colNames.indexOf("Address.Zip")]));
 			
-			Record record = new Record(Long.parseLong(input[colNames.indexOf("StudentID")]), Integer.parseInt(input[colNames.indexOf("Credits")]), 
-					Double.parseDouble(input[colNames.indexOf("GPA")]), input[colNames.indexOf("Major")], address);
 			
-			dict.add(record);
 		}
 		
 		return dict;
