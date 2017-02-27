f6bc5d5b50376d5da6af7cb94c28c3b76a880690
nmd001 <nmd001@lyn188-06>
2017-02-27 16:49:33 -0500

    k

--- Assignment 3/src/Main.java
+++ Assignment 3/src/Main.java
@@ -6,6 +6,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
@@ -29,14 +30,19 @@ public class Main {
 		while(in.ready()){
 			
 			//splits the next line by commas and keeps commas within quotes (example: "name, major, "street, city"" is split into "name", "major", "street, city")
-			
 			String[] input = in.readLine().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-				
+			
+			//map to be added in dict
+			Map<String, Object> map = new HashMap<String, Object>();
 			
 			Address address = new Address(input[colNames.indexOf("Address.Street")], input[colNames.indexOf("Address.city")], 
 					input[colNames.indexOf("Address.State")], Integer.parseInt(input[colNames.indexOf("Address.Zip")]));
 			
-			
+			 for(int i = 0; i < input.length; i++){
+				 //if the column name does not contain an address field, add the item to the map
+				 if(!colNames.get(i).contains("Address"))
+					 map.
+			 }
 		}
 		
 		return dict;
